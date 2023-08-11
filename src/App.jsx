dbb0132cae34b2687f90189924c190609e1e44be
CrispinWah <111033533+CrispinWah@users.noreply.github.com>
2023-08-11 12:41:35 -0400

    base css

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import "./my-sass.scss"
+
+export default function App() {
+  return  (
+  
+    <>
+  <header>
+  <div class="pattern">
+    <div class="container">
+      <div class="pattern-inner"></div>
+    </div>
+  </div>
+  
+  <div class="container">
+    <h1>
+      <span>Chance Leland Abenes </span>
+      <small> Artifacts</small>
+    </h1>
+  </div>
+</header>
+
+<main>
+  <div class="container">
+    <div class="content">
+      <p>
+       This is a rundown of all of my past projects. Enjoy!
+      </p>
+      
+      <button type="button">Hey, this is a button!</button>
+    </div>
+
+    <div class="content">
+      <p>
+       This is a rundown of all of my past projects. Enjoy!
+      </p>
+      
+      <button type="button">Hey, this is a button!</button>
+    </div>
+    
+    <div class="content">
+      <p>
+       This is a rundown of all of my past projects. Enjoy!
+      </p>
+      
+      <button type="button">Hey, this is a button!</button>
+    </div>
+
+    
+    
+    <div class="content">
+      <img src="https://basic-tutorials.de/wp-content/uploads/2016/08/NieR-Automata-Logo.jpg" width="300px"></img>
+    </div>
+  </div>
+</main>
+
+<div class="pattern">
+  <div class="container">
+    <div class="pattern-inner"></div>
+  </div>
+</div>
+      
+    
+  
+  </>
+ 
+  )
+}
